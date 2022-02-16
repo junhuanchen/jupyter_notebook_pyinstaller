6df95479eedeb08a8c0e0605f95dc95b390db163
Juwan <junhuanchen@qq.com>
2022-02-17 05:40:38 +0800

    Update build_win_exe.bat

--- build_win_exe.bat
+++ build_win_exe.bat
@@ -1,5 +1,7 @@
 powershell rd -Force -Recurse .\dist\maixpy3_notebook\
-pip install .\rpyc_ikernel\
+cd rpyc_ikernel
+pip install .
+cd ..
 pyinstaller maixpy3_notebook.spec -w --noconsole --clean --onefile --icon=app.ico
 powershell rd -Force -Recurse .\dist\maixpy3_notebook\share
 powershell copy -Force -Recurse .\dist\share .\dist\maixpy3_notebook
